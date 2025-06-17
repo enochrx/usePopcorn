a01a6b96fb492ef939f9a5069e3cc0f8b6cf9cf8
enochrx <drreayy@gmail.com>
2025-06-17 23:51:44 +0100

    added star rating component

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={10} />
   </StrictMode>
 );
