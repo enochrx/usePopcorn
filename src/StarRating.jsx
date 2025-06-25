6ef40750472be48c5477a73e20e9e58729fc5bde
enochrx <drreayy@gmail.com>
2025-06-25 09:00:37 +0100

    added star rating component

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -17,6 +17,7 @@ const StarRating = ({
   size = 48,
   message = [],
   defaultRating = 0,
+  onSetRating,
 }) => {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
@@ -28,13 +29,18 @@ const StarRating = ({
     fontSize: `${size / 1.5}px`,
   };
 
+  function handleRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
 // import "./index.css";
 import StarRating from "./StarRating";
+
+const CustomRating = () => {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="green" onSetRating={setMovieRating} maxRating={10} />
+      <p>This movie is rated {movieRating} stars on Moviebox and iMDb</p>
+    </div>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,5 +23,6 @@ createRoot(document.getElementById("root")).render(
       message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating maxRating={6} color="red" size={24} defaultRating={3} />
+    <CustomRating />
   </StrictMode>
 );
