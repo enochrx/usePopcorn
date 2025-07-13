42e365d8b92d491726cdc1772a8bd01ead820270
enochrx <drreayy@gmail.com>
2025-07-13 05:20:39 +0100

    children prop

--- src/App v2.jsx
+++ src/App v2.jsx
@@ -132,14 +132,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,28 @@
 import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-import StarRating from "./StarRating";
+import App from "./App.jsx";
+import "./index.css";
+// import StarRating from "./StarRating";
 
-const CustomRating = () => {
-  const [movieRating, setMovieRating] = useState(0);
+// const CustomRating = () => {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="green" onSetRating={setMovieRating} maxRating={10} />
-      <p>This movie is rated {movieRating} stars on Moviebox and iMDb</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <StarRating color="green" onSetRating={setMovieRating} maxRating={10} />
+//       <p>This movie is rated {movieRating} stars on Moviebox and iMDb</p>
+//     </div>
+//   );
+// };
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating maxRating={6} color="red" size={24} defaultRating={3} />
-    <CustomRating />
+    <CustomRating /> */}
   </StrictMode>
 );
